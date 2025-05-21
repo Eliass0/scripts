45acba7102b6b2edafb739700d0468d50f7afe7c
elias <schemmelelias@gmail.com>
2025-05-21 18:48:29 +0200

    made baterry only show up if exist

--- dwm-status.sh
+++ dwm-status.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 while true; do
-    battery=$(cat /sys/class/power_supply/BAT0/capacity)%[$(cat /sys/class/power_supply/BAT0/status)]
-    time=$(date +"%Y-%m-%d %H:%M")
-    xsetroot -name "$battery | $time"
-    # sleep $((60 - $(date +%S)))
+    if [ -e /sys/class/power_supply/BAT0/capacity ]; then
+        # Battery exists, display battery status
+        battery=$(cat /sys/class/power_supply/BAT0/capacity)%[$(cat /sys/class/power_supply/BAT0/status)]
+        status="$battery | $(date +"%Y-%m-%d %H:%M")"
+    else
+        # Battery doesn't exist, just display time
+        status="$(date +"%Y-%m-%d %H:%M")"
+    fi
+    xsetroot -name "$status"
     sleep 1
 done
-
