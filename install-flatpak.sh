5447182ef10530b2fa12ed9cf7b31f55c5988dda
elias <schemmelelias@gmail.com>
2025-05-23 09:25:46 +0200

    andriod studio

---
+++ install-flatpak.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# add Flatpak apps to PATH
+if [ -d "/var/lib/flatpak/exports/bin" ]; then
+    PATH="/var/lib/flatpak/exports/bin:$PATH"
+fi
+
+if [ -d "$HOME/.local/share/flatpak/exports/bin" ]; then
+     PATH="$HOME/.local/share/flatpak/exports/bin:$PATH"
+fi
+
+export PATH
